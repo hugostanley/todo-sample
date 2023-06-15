1b98ef04890d414d566a6ce347037e3da2927844
hugostanley <stanleyhugo06@gmail.com>
2023-06-15 19:13:26 +0800

    feat: add auth pages

---
+++ src/components/auth/Restricted.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { useLoaderData, Navigate } from 'react-router-dom'
+
+interface AuthenticateProps {
+  component: React.ReactNode
+}
+
+export default function Restricted({ component }: AuthenticateProps) {
+  const data = useLoaderData()
+
+  if (data !== false) {
+    return <Navigate to={"/"} />
+  }
+
+  return <>{component}</>
+}
+
+
+
